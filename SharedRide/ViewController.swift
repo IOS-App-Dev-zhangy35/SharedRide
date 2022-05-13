d31a09685204d7456defb505cfbbf2aebbe328c9
YIntong Zhang <zhangy35@rose-hulman.edu>
2022-05-13 14:09:02 -0400

    Initial Commit

---
+++ SharedRide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SharedRide
+//
+//  Created by 章音童 on 2022/5/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
